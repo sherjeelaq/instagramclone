5fe7400df2cb45feb487106213b80342f371899f
Sherjeel Afsar <sherjeelaq@gmail.com>
2020-12-14 21:42:29 +0500

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAjN2R2VqN7GlngssFurYbZYdei2CFxUyQ",
-  authDomain: "instagram-2ccdf.firebaseapp.com",
-  databaseURL: "https://instagram-2ccdf.firebaseio.com",
-  projectId: "instagram-2ccdf",
-  storageBucket: "instagram-2ccdf.appspot.com",
-  messagingSenderId: "490856549529",
-  appId: "1:490856549529:web:f1d6c4607747ed9641fc08",
-};
+const firebaseConfig = {};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
